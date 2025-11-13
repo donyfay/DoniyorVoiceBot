c4b7bdd48efbe2a2f75811b7651f586aff7a59cf
donyfay <don.fayziev@gmail.com>
2025-11-13 22:43:47 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-# Обеспечиваем, что скрипт исполняемый
-chmod +x main.py
-
-# Активируем виртуальное окружение Render
-source .venv/bin/activate
-
-# Обновляем pip
-pip install --upgrade pip
-
-# Устанавливаем зависимости
-pip install -r requirements.txt
-
-# Запуск бота
 python main.py
